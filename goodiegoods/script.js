0c5900322f680a5b2119bf4251bcd8249a9d64d1
BornKk <119116428+BornKk@users.noreply.github.com>
2024-10-23 20:14:40 +0700

    Add files via upload

---
+++ goodiegoods/script.js
@@ -0,0 +1,12 @@
+// SwiperJS initialization
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
